d6507e8644f9d6ee50637c0f0235a2dd549fb3c0
Agata Maciocha <agata.maciocha@gmail.com>
2014-02-02 19:21:53 +0100

    readed - fix problems

--- Program.cs
+++ Program.cs
@@ -7,23 +7,25 @@ namespace MyTSP
         static void Main()
         {
             double length1, length2, lengthTsp;
-            var data = new Coordinate[] {
-      new Coordinate(0, 16.47, 96.10),
-      new Coordinate(1, 16.47, 94.44),
-      new Coordinate(2, 20.09, 92.54),
-      new Coordinate(3, 22.39, 93.37),
-      new Coordinate(4, 25.23, 97.24),
-      new Coordinate(5, 22.00, 96.05),
-      new Coordinate(6, 20.47, 97.02),
-      new Coordinate(7, 17.20, 96.29),
-      new Coordinate(8, 16.30, 97.38),
-      new Coordinate(9, 14.05, 98.12),
-      new Coordinate(10, 16.53, 97.38),
-      new Coordinate(11, 21.52, 95.59),
-      new Coordinate(12, 19.41, 97.13),
-      new Coordinate(13, 20.09, 94.55)
-    };
-            var solver = new Solver(0, data);
+    //        var data = new Coordinate[] {
+    //  new Coordinate(0, 16.47, 96.10),
+    //  new Coordinate(1, 16.47, 94.44),
+    //  new Coordinate(2, 20.09, 92.54),
+    //  new Coordinate(3, 22.39, 93.37),
+    //  new Coordinate(4, 25.23, 97.24),
+    //  new Coordinate(5, 22.00, 96.05),
+    //  new Coordinate(6, 20.47, 97.02),
+    //  new Coordinate(7, 17.20, 96.29),
+    //  new Coordinate(8, 16.30, 97.38),
+    //  new Coordinate(9, 14.05, 98.12),
+    //  new Coordinate(10, 16.53, 97.38),
+    //  new Coordinate(11, 21.52, 95.59),
+    //  new Coordinate(12, 19.41, 97.13),
+    //  new Coordinate(13, 20.09, 94.55)
+    //};
+            var reader = new Reader();
+            var data = reader.ReadConsoleData();
+            var solver = new Solver(data[0].Name, data);
             var tour = solver.Solve2Tsp(out length1, out length2, out lengthTsp);
             Console.WriteLine("Cost = {0} + {1} \n Tsp = {2}", length1, length2, lengthTsp);
             Console.WriteLine("Tour:");
--- Reader.cs
+++ Reader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace MyTSP
 {
@@ -12,7 +13,19 @@ namespace MyTSP
             {
                 var line = Console.ReadLine();
                 var tab = line.Split(' ');
-                data[i] = new Coordinate(Convert.ToInt32(tab[0]), Convert.ToInt32(tab[1]), Convert.ToInt32(tab[2]));
+                double coord1;
+                double coord2;
+                try
+                {
+                    coord1 = Convert.ToDouble(tab[1], CultureInfo.InvariantCulture);
+                    coord2 = Convert.ToDouble(tab[2], CultureInfo.InvariantCulture);
+                    data[i] = new Coordinate(i/*Convert.ToInt32(tab[0])*/, coord1, coord2);
+                }
+                catch
+                {
+                    Console.WriteLine("i: {0}, line: {1}",i, line);
+                    break;
+                }
             }
             return data;
         }
